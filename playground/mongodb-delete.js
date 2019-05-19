dee5cd40196159c9d632a73c4fbf2a412885ac9d
Simranjeet <simranjeetsingh2009@gmail.com>
2019-05-19 07:51:00 +0530

    Update mongodb-delete.js

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -17,16 +17,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', {
 
     //deleteMany
 
-    // db.collection('Users').deleteMany({ name: 'Andrew' }).then((result) => {
-    //     console.log(result);
-    // });
+    db.collection('Users').deleteMany({ name: 'Andrew' }).then((result) => {
+        console.log(result);
+    });
 
 
     //findOneAndDelete
 
-    db.collection('Todos').findOneAndDelete({ text: 'Eat Lunch' }).then((result) => {
-        console.log(result);
-    });
+    // db.collection('Todos').findOneAndDelete({ text: 'Eat Lunch' }).then((result) => {
+    //     console.log(result);
+    // });
 
     client.close();
 
